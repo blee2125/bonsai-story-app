7a37b88d1dfc3d8375c99e562cbc994f1cf9e463
Brandon Lee <blee2125@gmail.com>
2021-08-26 16:37:58 -0400

    create entry card

---
+++ src/components/EntryCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+const EntryCard = (props) => {
+    const  entry  = props.entry;
+
+    return(
+        <div className="card-container">
+            <img src="" alt="" />
+            <div className="desc">
+                
+                <h3>{entry.notes}</h3>
+                <p>{entry.image_url}</p>
+            </div>
+        </div>
+    )
+};
+
+export default TreeCard;
